cd02882aaacf32ba983b7e1f070052c8382f9fd3
BeatriceCarru <80855260+BeatriceCarru@users.noreply.github.com>
2021-07-17 16:43:04 +0200

    modifica

--- app/src/main/java/com/example/a2ptrova/MainActivity.java
+++ app/src/main/java/com/example/a2ptrova/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a=10;
-        int be=a+5;
+
+        int c=95309;
     }
 }
